764eda3e0b5c4c48e987fee0126deaa278a78709
Jonathan <jjones646@gmail.com>
2014-11-23 22:20:29 -0500

    initial commit

---
+++ mbed/main.cpp
@@ -0,0 +1,241 @@
+#include "mbed.h"
+#include <string>
+#include <vector>
+
+// These are used for setting the serial buffer size and how long the mbed should wait between updates
+static const float SLEEP_TIME = 0.5;    // x86 background process will only have updates every 1 second in ideal situations
+static const float PIN_DELAY = 0.05;   // the time to wait for allowing the relays to fully cycle between transitions
+static const float POWER_UP_TIME = 5.0; // seconds for the device clicker to go from an OFF to ON state
+static const int NUM_CMDS = 8;
+		
+// Used for signaling to the main loop if new serial data has arrived by the serial rx interrupt		
+bool new_data;
+
+// Enumerations for the device's power state
+enum device_state_t {
+    ON = 1,
+    OFF = 2
+};
+
+// Object for linking to a computer via serial communication
+Serial pc(USBTX, USBRX);
+
+// Global string for storing the received serial data on the serial rx interrupt
+string gStr;
+
+
+void rx_interrupt(void)
+{
+    int i = 0;
+
+    // update global character array
+    while (pc.readable() | (i<6) ) {
+        gStr += pc.getc();
+			  i++;  
+			  wait(0.02);
+    }
+		
+		new_data = 1;
+}
+
+
+void make_edge(DigitalOut& pin)
+{
+    pin = !pin;
+    wait(PIN_DELAY);
+    pin = !pin;
+}
+
+
+void confirm_on(AnalogIn& ain){
+	uint16_t readings[3];
+	
+	// get 5 initial readings
+	for (int i=0; i<4; i++)
+	{
+		readings[i] = ain.read_u16();
+		wait(0.01);
+	}
+	
+	for (int i=0; i<4; i++)
+	{
+			if (readings[i] < 60000)
+			{
+				  confirm_on(ain);	// break into recursion if past readings are not settled values
+			}
+	}
+}
+
+
+int main()
+{
+    // Blinking LED to show that mbed is running
+    DigitalOut led1(LED1);
+
+    // On/Off, Mode, Send
+    DigitalOut controls[] = { p13, p14, p15 };
+
+    // A, B, C, D, E
+    DigitalOut selections[] = { p16, p17, p18, p19, p21 };
+		
+		// Used for reading the clicker's voltage regulator output for determining if clicker is on/off
+		AnalogIn pwr_status(p20);
+    
+		// Define the commands variable that holds a list of all valid serial commands
+		std::vector<string> commands(NUM_CMDS);
+		
+		// Define the valid set of serial commands
+    commands[0] = "clickerPower\n";
+		commands[1] = "clickerSend\n";
+		commands[2] = "sayHere\n";
+		commands[3] = "clickerA\n";
+		commands[4] = "clickerB\n";
+		commands[5] = "clickerC\n";
+		commands[6] = "clickerD\n";
+		commands[7] = "clickerE\n";
+    
+    // Initialize power toggle pin so clicker stays off at mbed startup
+    controls[0] = 1;
+		
+		// get initial power readings
+		uint16_t pwr_level = pwr_status.read_u16();
+	  uint16_t pwr_level_new = pwr_status.read_u16();
+		
+    device_state_t dev_power = ON;    // initialize to an off state
+		
+		// variables for the main scope
+    char current_selection = NULL;
+    bool update_selection = false;
+		int usr_cmd = 0;
+		
+		// Holding down active low Power button will put clicker in configuration mode...we don't want that
+		selections[0] = 1;
+		
+		// Function to run at receiving serial data
+    pc.attach(&rx_interrupt, Serial::RxIrq);
+		
+    // infinite processing loop
+    while(1) {
+
+        // pull in from globally allocated memory when interurpts are disabled
+        __disable_irq();
+			
+			pwr_level_new = pwr_status.read_u16();
+			
+			if (new_data){
+				new_data = 0;
+			
+			// assign a valid command to variable for the switch case statement. If command is invalid, ignore
+			for (int i=0; i<NUM_CMDS+1; i++){
+					if ( !(strcmp(gStr.c_str(), commands[i].c_str())) ) {
+						  usr_cmd = i+1;
+						  break;
+					}
+		    }
+			
+        switch( usr_cmd ) {
+            case 1: // clickerPower
+                make_edge(controls[0]);
+                break;
+
+            case 2:	// clickerSend
+                if(current_selection) {
+                    make_edge(controls[1]);
+                    pc.printf("    Answer submitted: %c\r\n", current_selection);
+                    current_selection = NULL;
+                } else {
+                    pc.printf("    Must make a selection before attempting a submission\r\n");
+                }
+								make_edge(controls[1]);	// comment line out for final version. Used for testing purposes here
+                break;
+
+            case 3:  // sayHere
+								pc.printf("    Audio currently unsupported\r\n");
+                break;
+
+            case 4:	// clickerA
+                current_selection = 'A';
+                update_selection = true;
+                break;
+
+            case 5:	// clickerB
+                current_selection = 'B';
+                update_selection = true;
+                break;
+
+            case 6: // clickerC
+                current_selection = 'C';
+                update_selection = true;
+                break;
+
+            case 7: // clickerD
+                current_selection = 'D';
+                update_selection = true;
+                break;
+
+            case 8: // clickerE
+                current_selection = 'E';
+                update_selection = true;
+                break;
+
+            default:
+                break;
+        }
+			}
+
+			
+        if (update_selection) {
+					
+					update_selection = false;
+					
+					if (dev_power == ON){
+					  make_edge(selections[usr_cmd - 4]);					
+					  pc.printf("    You have selected option %c\r\n", current_selection);
+						
+					} else {
+						pc.printf("    Device is not on\r\n");
+						current_selection = NULL;
+					}
+        }
+				
+				// if clicker's voltage level has changed significantly...
+				if ( abs(pwr_level_new - pwr_level) > 20000)
+				{
+					
+					// settling time
+					while ( abs(pwr_level_new - pwr_status.read_u16()) > 32 )
+					{
+						// wait until the analog input settles to a steady state reading
+						pwr_level_new = pwr_status.read_u16();
+						wait(0.1);
+					}
+					
+					if (pwr_level_new < pwr_level)	// device is shutting down
+					{
+						dev_power = OFF;
+						
+					} else if (pwr_level_new > pwr_level)	// device is turning on
+					{
+						confirm_on(pwr_status);		// wait for voltage levels to settle to steady value when powering up
+						wait(POWER_UP_TIME);
+						dev_power = ON;
+					}
+
+					  // update the power level reading
+					  pwr_level = pwr_level_new;
+            pc.printf("    Device power status updated. New status: %s\r\n", dev_power == ON ? "ON": "OFF");
+				}
+				
+				
+				// clear all command related variables
+				gStr.clear();
+				usr_cmd = 0;
+					
+        // turn interrupts back on before going into a sleeping state
+        __enable_irq();
+
+        // blink that shit then be lazy
+        led1 = !led1;
+        wait(SLEEP_TIME);
+    }
+}
---
+++ x86Host/AttendanceBot5000/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.IO.Ports;
+
+namespace AttendanceBot5000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Booting Attendance Bot 5000");
+            Console.WriteLine("===========================\n");
+            // Get a list of serial port names. 
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine("The following serial ports were found:");
+
+            // Display each port name to the console. 
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+            }
+            Console.WriteLine("======================================");
+            // set up serial port
+            SerialPort _serialPort = new SerialPort();
+            _serialPort.ReadTimeout = 10000;
+            _serialPort.WriteTimeout = 500;
+            _serialPort.BaudRate = 9600;
+
+            while(!_serialPort.IsOpen){
+                try
+                {
+                    Console.WriteLine("\nPlease type the name of your desired port:");
+                    string choosePort = Console.ReadLine();
+                    _serialPort.PortName = choosePort;
+                    _serialPort.Open();
+                    Console.WriteLine("======================================\n");
+                    Console.WriteLine("Connected! Waiting for user commands...");
+                } catch (System.IO.IOException e) {
+                    Console.WriteLine("Error connecting with mbed ({0}).", e.Message);
+                }
+            }
+            
+            WebClient Client = new WebClient();
+            Client.DownloadFile("http://104.131.69.134/commands.txt", "commands.txt");
+            
+            string[] commandsInit = File.ReadAllLines("commands.txt");
+            string oldLastCommand = commandsInit[commandsInit.Length - 1];
+
+            while (true)
+            {
+                Client.DownloadFile("http://104.131.69.134/commands.txt", "commands.txt");
+                string[] commands = File.ReadAllLines("commands.txt");
+                string newLastCommand = commands[commands.Length - 1];
+
+                if(newLastCommand == oldLastCommand){
+                    System.Threading.Thread.Sleep(1000);
+                    continue;
+                }
+                else{
+
+                    oldLastCommand = newLastCommand;
+                    string[] splitCommand = newLastCommand.Split(',');
+
+                    Console.WriteLine("\n========================= {0}", DateTime.Now.ToString("hh:mm:ss tt"));
+                    Console.WriteLine(splitCommand[1]);
+                    _serialPort.WriteLine(splitCommand[1]);
+
+                    try
+                    {
+                        string message = _serialPort.ReadLine();
+                        Console.WriteLine(message);
+                    } catch (System.TimeoutException e) {
+                        Console.WriteLine("    mbed did not respond ({0}).", e.Message);
+                    }
+                    
+                    System.Threading.Thread.Sleep(500);
+                }
+            }
+        }
+    }
+}
